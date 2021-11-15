0f11b46f5e096f4da656c0d6e4969220422e2223
Dakdaman <93922559+Dakdaman@users.noreply.github.com>
2021-11-15 14:48:52 +0200

    fff
    ffff

---
+++ main.py
@@ -0,0 +1,38 @@
+
+def izglītītbas_programas():
+    #saruna par V2V Direktoriem
+    print ("Viņi ir ļoti atbalstoeši")
+    pass
+
+def Direktori():
+    #saruna par V2V
+
+
+
+
+#Čatbots par skolas vēsturi
+def izvelne(vārds):
+    print(vārds + "Izvēlies par ko gribi uzzināt? Izvēlies un ievadi vērtību ")
+    x = input("1 - Izglītības programas\n2-Direktori \n3-Tradīcijas \n4-Skolas ēka")
+    lietotāj_izvēle = int (lietotāj_izvēle)
+    if lietotāj_izvēle == 1:
+        Izglītības_programas(0)
+    if lietotāj_izvēle == 2
+        Direktori()
+    if lietotāj_izvēle == 3
+        Tradīcijas()
+    if lietotāj_izvēle == 4
+        Skolas_ēka()
+
+
+
+
+
+def print_hi(name):
+
+
+
+
+
+
+
